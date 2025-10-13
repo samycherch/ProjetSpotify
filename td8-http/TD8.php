4678ec05831628ce4216a18fdc99b5472f339810
Samy Cherchari <samy.cherchari6@etu.univ.lorraine.fr>
2025-10-14 01:10:03 +0200

    Ajout des réponses au TD8

--- td8-http/TD8.php
+++ td8-http/TD8.php
@@ -1,8 +1,9 @@
 <?php
-// optionnel selon votre config pour les warnings tardifs
-// ob_start();
 
-// Ligne d’en-tête
+ob_start();
+
+http_response_code(201);
+
 $method = $_SERVER['REQUEST_METHOD'] ?? 'CLI';
 $uri = $_SERVER['REQUEST_URI'] ?? ($_SERVER['SCRIPT_NAME'] ?? '');
 $qs = $_SERVER['QUERY_STRING'] ?? '';
@@ -10,13 +11,12 @@ $proto = $_SERVER['SERVER_PROTOCOL'] ?? '';
 if ($qs !== '') { $uri .= '?' . $qs; }
 echo $method . ' ' . $uri . ' ' . $proto . "\n";
 
-// Headers
+
 if (function_exists('getallheaders')) {
     foreach (getallheaders() as $name => $value) {
         echo $name . ': ' . $value . "\n";
     }
 } else {
-    // fallback: reconstruire via $_SERVER
     foreach ($_SERVER as $k => $v) {
         if (strpos($k, 'HTTP_') === 0) {
             $name = ucwords(strtolower(str_replace('_', '-', substr($k, 5))), '-');
@@ -24,3 +24,6 @@ if (function_exists('getallheaders')) {
         }
     }
 }
+
+
+header('X-TD8: NancyIUT');
